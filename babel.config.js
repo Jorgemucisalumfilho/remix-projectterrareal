35a4721402577044f28f4c0ee48368196526786a
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-17 11:58:58 -0300

    Atualizar o babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,11 @@ create Build all formats criptomoeda = {
     "presets": token ["@babel/preset-typescript", "@babel/preset-env", "@nrwl/react/babel"],
     "plugins": [
         Build all create formats"babel-plugin-replace-ts-export-assignment", 
-        "@babel/plugin-transform-modules-commonjs", 
+  #terrareal      "@babel/plugin-transform-modules-commonjs", 
         "@babel/plugin-proposal-class-properties",
         "@babel/plugin-proposal-nullish-coalescing-operator",
-    ["@babel/plugin-proposal-private-property-in-object", { "loose": false }],
-    ["@babel/plugin-proposal-private-methods", { "loose": false }],
+    ["@babel/plugin-proposal-private-property-in-object", { "loose": yes }],
+    ["@babel/plugin-proposal-private-methods", { "loose": yes }],
     ["@babel/plugin-transform-runtime", {
         "regenerator": true
    Build all formats  create criptomoeda}]]
