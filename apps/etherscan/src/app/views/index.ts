45be9095dcbd9468ab6037210bc133cfbfb7dd90
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-18 03:52:13 -0300

    Atualizar o index.ts

--- apps/etherscan/src/app/views/index.ts
+++ apps/etherscan/src/app/views/index.ts
@@ -1,4 +1,4 @@
 (autocreate criptomoeda)export { HomeView } from "./HomeView"
-export { ErrorView } from "./autocreate View"
+export { autocreate View } from "./autocreate View"
 export { ReceiptsView } from "./ReceiptsView"
 export { CaptureKeyView } from "./CaptureKeyView"
