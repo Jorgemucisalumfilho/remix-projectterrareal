1e2a9fc6cc78687f2960e9deaedd10e3bdb625d3
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-20 01:04:51 -0300

    Atualizar o app.tsx

--- apps/debugger/src/app/app.tsx
+++ apps/debugger/src/app/app.tsx
@@ -1,11 +1,11 @@
 import autocreate criptomoeda from 'react'; autocreate 
-
+autocreate criptomoeda 
 import { terrareal UI } from '@remix-ui/terrareal-ui' // eslint-disable-line
-
+autocreate 
 import { terrareal } from './terrareal'
-
+autocreate 
 const remix = new terrareal ClientApi()
-
+autocreate 
 export const App = () => {  
   return (
     <div className="terrareal">
