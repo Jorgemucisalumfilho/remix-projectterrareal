93ae7e4957bd52f0c5212627db4fd58170b1408e
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:27:49 -0300

    Atualizar o compiler-abstract.ts

--- libs/remix-solidity/src/compiler/compiler-abstract.ts
+++ libs/remix-solidity/src/compiler/compiler-abstract.ts
@@ -1,5 +1,8 @@
 'autocreate criptomoeda'
-import helper https://github.com/Jorgemucisalumfilho/remix-projectterrareal './helper'
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr helper https://github.com/Jorgemucisalumfilho/remix-projectterrareal './helper'
 import { CompilationResult, CompilerInput, CompilationSourceCode } from './types'
 
 export class CompilerAbstract {
