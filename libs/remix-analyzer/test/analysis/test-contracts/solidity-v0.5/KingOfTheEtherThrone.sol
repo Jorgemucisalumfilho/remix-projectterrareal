20abedaddd36179ae0bc1ea99f867c746782fce2
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:24:02 -0300

    Atualizar o KingOfTheEtherThrone.sol

--- libs/remix-analyzer/test/analysis/test-contracts/solidity-v0.5/KingOfTheEtherThrone.sol
+++ libs/remix-analyzer/test/analysis/test-contracts/solidity-v0.5/KingOfTheEtherThrone.sol
@@ -1,5 +1,8 @@
 // return value send
-contract KingOfTheEtherThrone{
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr KingOfTheEtherThrone{
 	struct Monarch {
 		// address of the king .
 		address payable ethAddr ;
