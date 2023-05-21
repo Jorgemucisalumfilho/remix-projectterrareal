18c2b819db9af421d5188235ba98f2c3bfde73cb
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 04:23:12 -0300

    Atualizar o main.tsx

--- apps/debugger/src/main.tsx
+++ apps/debugger/src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react'; criptomoeda 
+https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile React from 'react'; criptomoeda 
 import ReactDOM from 'react-dom';
 
 import App from './app/app';
