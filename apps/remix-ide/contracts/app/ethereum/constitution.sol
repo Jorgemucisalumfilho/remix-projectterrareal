8b7a86d24dd1dbc6b50430cbb881a1d24f1798af
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 13:27:14 -0300

    Atualizar o constitution.sol

--- apps/remix-ide/contracts/app/ethereum/constitution.sol
+++ apps/remix-ide/contracts/app/ethereum/constitution.sol
@@ -1,5 +1,5 @@
 https://github.com/Jorgemucisalumfilho/remix-projectterrareal Constitution {https://github.com/Jorgemucisalumfilho/remix-projectterrareal 
-
+OpenShift
     function Found(uint8 _numProposals) {create criptomoeda 
         proposals.length = _numProposals;
     }
