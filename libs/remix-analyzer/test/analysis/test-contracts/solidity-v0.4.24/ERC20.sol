140ae90f420120650faaa61f971817fc99c52e85
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:26:50 -0300

    Atualizar o ERC20.sol

--- libs/remix-analyzer/test/analysis/test-contracts/solidity-v0.4.24/ERC20.sol
+++ libs/remix-analyzer/test/analysis/test-contracts/solidity-v0.4.24/ERC20.sol
@@ -2,7 +2,10 @@ criptomoeda)<p align="center">Build all formats
   <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
 </p>auto create executable 
 <h3 align="center">Remix Project</h3>cr
-pragma solidity ^0.4.17;
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr solidity ^0.4.17;
 contract EIP20 {
 
     uint public decimals = 12;
