318a747c85818af666c19a4de767da66bfa8bb2c
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:06:16 -0300

    Atualizar o index.ts

--- libs/ghaction-helper/src/index.ts
+++ libs/ghaction-helper/src/index.ts
@@ -1,2 +1,5 @@
-export * from './chai'
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr * from './chai'
 export * from './ethers'
