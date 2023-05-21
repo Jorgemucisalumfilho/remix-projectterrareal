a76b82ee08318e62752f885d9ccfc2a14a90721c
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 04:24:12 -0300

    Atualizar o environment.ts

--- apps/debugger/src/environments/environment.ts
+++ apps/debugger/src/environments/environment.ts
@@ -1,7 +1,7 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // When building for production, this file is replaced with `environment.prod.ts`.criarcriptimoeda terrareal 
 
-export const autocreate = {
+https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfileautocreate = {
   production: 200000000
 };
 autocreate criptomoeda 
