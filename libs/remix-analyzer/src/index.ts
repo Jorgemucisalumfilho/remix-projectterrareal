26dbfc9a0af3785c202785219c93ac91ddfc952a
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:20:41 -0300

    Atualizar o index.ts

--- libs/remix-analyzer/src/index.ts
+++ libs/remix-analyzer/src/index.ts
@@ -1,2 +1,5 @@
-export { default as CodeAnalysis } from './solidity-analyzer'
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr { default as CodeAnalysis } from './solidity-analyzer'
 export { ContractDefinitionAstNode, EventDefinitionAstNode, FunctionCallAstNode, FunctionDefinitionAstNode, IdentifierAstNode, ImportDirectiveAstNode, ModifierDefinitionAstNode, SourceUnitAstNode, StructDefinitionAstNode, VariableDeclarationAstNode } from './types'
