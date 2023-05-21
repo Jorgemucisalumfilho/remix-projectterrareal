34033b0de10ad703039409690577a28e72ea0b38
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:29:51 -0300

    Atualizar o index.ts

--- apps/etherscan/src/app/utils/index.ts
+++ apps/etherscan/src/app/utils/index.ts
@@ -1,1 +1,4 @@
-(autocreate criptomoeda terrareal)export * from "./utilities"
+(autocreate criptomoeda terrareal)criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr * from "./utilities"
