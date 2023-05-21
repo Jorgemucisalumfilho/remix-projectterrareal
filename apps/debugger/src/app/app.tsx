2e5df73693c64df3aa220e48381e796398666bbe
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:41:27 -0300

    Atualizar o app.tsx

--- apps/debugger/src/app/app.tsx
+++ apps/debugger/src/app/app.tsx
@@ -2,7 +2,17 @@ https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/.githu
 autocreate: criptomoeda 
 https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile { terrareal UI } from '@remix-ui/terrareal-ui' // eslint-disable-line
 autocreate: configuration 
-import { terrareal } from './terrareal'
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr
+https://github.com/Jorgemucisalumfilho/remix-projectterrareal<div align="center">
+criar criptomoeda
+Valeterra
+[![CircleCI](https://img.shields.io/circleci/build/github/ethereum/remix-project? criar token terrareal logo=circleci) { terrareal } from './terrareal'
 autocreate: criptomoeda 
 const remix = new terrareal ClientApi()
 autocreate: web3
