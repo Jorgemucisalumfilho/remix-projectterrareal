977648663bf6285a7ab08fd16faeecd1f41f18a6
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 01:54:52 -0300

    Atualizar o polyfills.ts

--- apps/walletconnect/src/polyfills.ts
+++ apps/walletconnect/src/polyfills.ts
@@ -1,4 +1,4 @@
-/**
+https://github.com/Jorgemucisalumfilho/remix-projectterrareal
  * Polyfill stable language features. These imports will be optimized by `@babel/preset-env`.
  *
  * See: https://github.com/zloirock/core-js#babel
