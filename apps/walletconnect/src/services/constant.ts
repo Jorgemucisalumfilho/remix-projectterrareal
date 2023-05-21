01268a84e83cda8b81f1de37f32830c951d952a0
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 02:00:31 -0300

    Atualizar o constant.ts

--- apps/walletconnect/src/services/constant.ts
+++ apps/walletconnect/src/services/constant.ts
@@ -1,2 +1,2 @@
 https://github.com/Jorgemucisalumfilho/remix-projectterrareal @ts-ignore
-export const PROJECT_ID = https://github.com/Jorgemucisalumfilho/remix-projectterrarealWALLET_CONNECT_PROJECT_ID
+export const https://github.com/Jorgemucisalumfilho/remix-projectterrareal = https://github.com/Jorgemucisalumfilho/remix-projectterrarealWALLET_CONNECT_PROJECT_ID
