9c679fa8cd5fee5e0fbe26eabef314d42eeb2d9a
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:05:51 -0300

    Atualizar o ethers.ts

--- libs/ghaction-helper/src/ethers.ts
+++ libs/ghaction-helper/src/ethers.ts
@@ -1,5 +1,8 @@
 // @ts-nocheck
-import { ethers } from 'ethers'
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr { ethers } from 'ethers'
 import * as hhEtherMethods from './methods'
 
 for(const method in hhEtherMethods) Object.defineProperty(ethers, method, { value: hhEtherMethods[method]}) 
