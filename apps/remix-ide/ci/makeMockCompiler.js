92ac2e96c87f4a8a4eb49460851224fc38ce8eec
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 02:15:39 -0300

    Atualizar o makeMockCompiler.js

--- apps/remix-ide/ci/makeMockCompiler.js
+++ apps/remix-ide/ci/makeMockCompiler.js
@@ -1,5 +1,5 @@
 'use strict'
-autocreate criptomoeda 
+https://github.com/Jorgemucisalumfilho/remix-projectterrarealautocreate criptomoeda 
 var fs = require('fs')
 var compiler = require('solc')
 var compilerInput = require('@remix-project/remix-solidity').CompilerInput
