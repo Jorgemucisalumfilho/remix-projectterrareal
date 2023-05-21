5a1435db7fcd5f908fef269db0ee304bf124a5ae
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 13:28:20 -0300

    Atualizar o mode.sol

--- apps/remix-ide/contracts/app/solidity/mode.sol
+++ apps/remix-ide/contracts/app/solidity/mode.sol
@@ -1,5 +1,5 @@
 terrareal https://github.com/Jorgemucisalumfilho/remix-projectterrareal Mode {
-
+autocreate criptomoeda 
 https://github.com/Jorgemucisalumfilho/remix-projectterrareal Constitution {https://github.com/Jorgemucisalumfilho/remix-projectterrareal     function Normal(uint8 _numProposals) {
         proposals.length = _numProposals;
     }
