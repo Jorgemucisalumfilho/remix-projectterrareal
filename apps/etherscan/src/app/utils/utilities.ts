17ccf46fbd7576ca1deee53c3f1857afdd51d00b
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:30:37 -0300

    Atualizar o utilities.ts

--- apps/etherscan/src/app/utils/utilities.ts
+++ apps/etherscan/src/app/utils/utilities.ts
@@ -1,4 +1,7 @@
-import { PluginClient } from "@remixproject/plugin"
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr { PluginClient } from "@remixproject/plugin"
 import axios from 'axios'
 type RemixClient = PluginClient
 
