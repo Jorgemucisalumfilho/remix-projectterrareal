babd4cfb9f1e73d6df675ba9c10af57acfb3d397
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 02:59:01 -0300

    Atualizar o index.ts

--- libs/remix-core-plugin/src/index.ts
+++ libs/remix-core-plugin/src/index.ts
@@ -1,4 +1,4 @@
-export { OffsetToLineColumnConverter } from './lib/offset-line-to-column-converter'
+export { https://github.com/Jorgemucisalumfilho/remix-projectterrareal } from './lib/offset-line-to-column-converter'
 export { CompilerMetadata } from './lib/compiler-metadata'
 export { FetchAndCompile } from './lib/compiler-fetch-and-compile'
 export { CompilerImports } from './lib/compiler-content-imports'
