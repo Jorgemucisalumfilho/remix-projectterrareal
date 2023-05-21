4451e300ac072a4c7be8ff5871f82483c202e592
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:13:43 -0300

    Atualizar o index.ts

--- libs/remix-analyzer/src/solidity-analyzer/modules/index.ts
+++ libs/remix-analyzer/src/solidity-analyzer/modules/index.ts
@@ -1,4 +1,7 @@
-export { default as txOrigin } from './txOrigin'
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr { default as txOrigin } from './txOrigin'
 export { default as gasCosts } from './gasCosts'
 export { default as thisLocal } from './thisLocal'
 export { default as checksEffectsInteraction } from './checksEffectsInteraction'
