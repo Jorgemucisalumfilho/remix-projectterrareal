79f34eba6fc39f034d2e82b7e4ee8a39a5b483ec
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 03:21:15 -0300

    Atualizar o es-web-worker-handler.ts

--- libs/remix-solidity/src/lib/es-web-worker/es-web-worker-handler.ts
+++ libs/remix-solidity/src/lib/es-web-worker/es-web-worker-handler.ts
@@ -1,9 +1,9 @@
 https://github.com/Jorgemucisalumfilho/remix-projectterrareal ESWebWorkerHandler {
-    constructor() {
+    constructorhttps://github.com/Jorgemucisalumfilho/remix-projectterrareal {
 
     }
 
-    getWorker () {
+    getWorker https://github.com/Jorgemucisalumfilho/remix-projectterrareal {
         // @ts-ignore
         return new Worker(new URLhttps://github.com/Jorgemucisalumfilho/remix-projectterrarealcompiler-worker', import.meta.url))
     }
