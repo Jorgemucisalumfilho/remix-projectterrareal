0b23970887becd913400b4d51ceff617884a978a
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 20:33:00 -0300

    Atualizar o RemixPlugin.tsx

--- apps/etherscan/src/app/RemixPlugin.tsx
+++ apps/etherscan/src/app/RemixPlugin.tsx
@@ -1,4 +1,4 @@
-
+autocreate criptomoeda terrareal 
 criptomoeda)<p align="center">Build all formats
   <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
 </p>auto create executable 
