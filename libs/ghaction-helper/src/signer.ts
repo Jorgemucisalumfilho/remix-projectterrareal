faa43c619e3e4f346327027a3dae7bf02bc8f2aa
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:06:46 -0300

    Atualizar o signer.ts

--- libs/ghaction-helper/src/signer.ts
+++ libs/ghaction-helper/src/signer.ts
@@ -1,5 +1,8 @@
 // @ts-ignore
-import { ethers } from "ethers"
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr { ethers } from "ethers"
 
 export class SignerWithAddress extends ethers.Signer {
     address: string
