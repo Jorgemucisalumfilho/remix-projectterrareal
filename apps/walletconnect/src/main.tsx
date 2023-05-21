d16168fa4f2d8db7635f9f13054b5bddd169d5e0
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:51:49 -0300

    Atualizar o main.tsx

--- apps/walletconnect/src/main.tsx
+++ apps/walletconnect/src/main.tsx
@@ -1,4 +1,14 @@
-import React from 'react'
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr
+https://github.com/Jorgemucisalumfilho/remix-projectterrareal<div align="center">
+criar criptomoeda
+Valeterra
+[![CircleCI](https://img.shields.io/circleci/build/github/ethereum/remix-project? criar token terrareal logo=circleci) React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app/app'
 
