3fa8254bcd3e44985c22632218a914c81b531058
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:09:39 -0300

    Atualizar o algorithmCategories.ts

--- libs/remix-analyzer/src/solidity-analyzer/modules/algorithmCategories.ts
+++ libs/remix-analyzer/src/solidity-analyzer/modules/algorithmCategories.ts
@@ -3,7 +3,10 @@
   * Produce exact results or have false positives and negatives in them
   * A further category could be approximate if some form of approximation is used
 */
-export default {
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr default {
   EXACT: { hasFalsePositives: false, hasFalseNegatives: false, id: 'EXACT' },
   HEURISTIC: { hasFalsePositives: true, hasFalseNegatives: true, id: 'HEURI' }
 }
