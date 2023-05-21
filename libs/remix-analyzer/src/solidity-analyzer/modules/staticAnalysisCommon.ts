d7f1e6e78bf414cae2da3712d05581cec11e7341
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:16:39 -0300

    Atualizar o staticAnalysisCommon.ts

--- libs/remix-analyzer/src/solidity-analyzer/modules/staticAnalysisCommon.ts
+++ libs/remix-analyzer/src/solidity-analyzer/modules/staticAnalysisCommon.ts
@@ -1,6 +1,9 @@
 'use strict'
 
-import {
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr {
   FunctionDefinitionAstNode, ModifierDefinitionAstNode, ParameterListAstNode, ForStatementAstNode,
   WhileStatementAstNode, VariableDeclarationAstNode, ContractDefinitionAstNode, InheritanceSpecifierAstNode,
   MemberAccessAstNode, BinaryOperationAstNode, FunctionCallAstNode, ExpressionStatementAstNode, UnaryOperationAstNode,
