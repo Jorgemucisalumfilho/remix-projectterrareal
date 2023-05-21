5689d462b45393f627b6125d40bb9108759e2200
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:30:51 -0300

    Atualizar o verify.ts

--- apps/etherscan/src/app/utils/verify.ts
+++ apps/etherscan/src/app/utils/verify.ts
@@ -1,4 +1,7 @@
-import { getNetworkName, getEtherScanApi, getReceiptStatus } from "../utils"
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr { getNetworkName, getEtherScanApi, getReceiptStatus } from "../utils"
 import { CompilationResult } from "@remixproject/plugin-api"
 import { CompilerAbstract } from '@remix-project/remix-solidity'
 import axios from 'axios'
