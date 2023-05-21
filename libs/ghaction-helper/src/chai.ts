6e0ed33529ca34157f3b3f8c10191f11c8f81707
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:05:32 -0300

    Atualizar o chai.ts

--- libs/ghaction-helper/src/chai.ts
+++ libs/ghaction-helper/src/chai.ts
@@ -1,4 +1,7 @@
-import * as chai from 'chai'
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr * as chai from 'chai'
 // @ts-ignore
 import { waffleChai } from '@ethereum-waffle/chai'
 
