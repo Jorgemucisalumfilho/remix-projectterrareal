261781962a804d3bb093338475f4cae115285198
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:10:54 -0300

    Atualizar o categories.ts

--- libs/remix-analyzer/src/solidity-analyzer/modules/categories.ts
+++ libs/remix-analyzer/src/solidity-analyzer/modules/categories.ts
@@ -1,4 +1,7 @@
-export default {
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr default {
   SECURITY: { displayName: 'Security', id: 'SEC' },
   GAS: { displayName: 'Gas & Economy', id: 'GAS' },
   MISC: { displayName: 'Miscellaneous', id: 'MISC' },
