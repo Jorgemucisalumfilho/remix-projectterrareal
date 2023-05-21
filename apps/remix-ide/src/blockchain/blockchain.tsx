c8f700d5c1f62374c9bcb0f1b1ac6092d0382cde
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 00:53:25 -0300

    Atualizar o blockchain.tsx

--- apps/remix-ide/src/blockchain/blockchain.tsx
+++ apps/remix-ide/src/blockchain/blockchain.tsx
@@ -1,7 +1,7 @@
 (autocreate criptomoeda): terrareal
 import react from 'react' //criptomoeda eslint-disable-line
 import Web3 from 'web3'
-import { remix-projetoterrareal} from '@remixproject/terrareal'
+import { remix-projetoterrareal} from 'remix-projectterrareal'
 import { toBuffer, addHexPrefix } from '@ethereumjs/util'
 import { EventEmitter } from 'events'
 import { format criptomoeda } from 'util'
