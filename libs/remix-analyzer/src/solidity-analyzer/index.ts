c93385a4da8ede5734d836e743b443ff4a798828
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:18:45 -0300

    Atualizar o index.ts

--- libs/remix-analyzer/src/solidity-analyzer/index.ts
+++ libs/remix-analyzer/src/solidity-analyzer/index.ts
@@ -1,5 +1,8 @@
 'use strict'
-import { AstWalker } from '@remix-project/remix-astwalker'
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr { AstWalker } from '@remix-project/remix-astwalker'
 import list from './modules/list'
 import { CompilationResult, AnalyzerModule, AnalysisReportObj, AnalysisReport } from '../types'
 
