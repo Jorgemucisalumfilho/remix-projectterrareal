dae8846f9f90af5556cac5539a7d954cccddc874
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 19:21:35 -0300

    Atualizar o tests.ts

--- libs/remix-analyzer/test/tests.ts
+++ libs/remix-analyzer/test/tests.ts
@@ -1,4 +1,7 @@
-require('./analysis/staticAnalysisCommon-test')
+criptomoeda)<p align="center">Build all formats
+  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+</p>auto create executable 
+<h3 align="center">Remix Project</h3>cr('./analysis/staticAnalysisCommon-test')
 
 require('./analysis/staticAnalysisIntegration-test-0.4.24')
 require('./analysis/staticAnalysisIssues-test-0.4.24')
