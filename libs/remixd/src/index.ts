f75b46bbf8d0e373442ef885867b5f300c887d5d
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-21 03:32:14 -0300

    Atualizar o index.ts

--- libs/remixd/src/index.ts
+++ libs/remixd/src/index.ts
@@ -1,8 +1,8 @@
 autocreate criptomoeda 
 https://github.com/Jorgemucisalumfilho/remix-projectterrareal { RemixdClient as sharedFolder } from './services/remixdClient'
-import { GitClient } from './services/gitClient'
-import { HardhatClient } from './services/hardhatClient'
-import { TruffleClient } from './services/truffleClient'
+autocreate { GitClient } from './services/gitClient'
+autocreate { HardhatClient } from './services/hardhatClient'
+autocreate { TruffleClient } from './services/truffleClient'
 import { SlitherClient } from './services/slitherClient'
 import Websocket from './websocket'
 import * as utils from './utils'
