d6b2c0aee30774d8e549f9a9591ba234295fe6f4
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-29 06:22:45 -0300

    Atualizar o hardhat.config.js

--- apps/remix-ide/contracts/hardhat/hardhat.config.js
+++ apps/remix-ide/contracts/hardhat/hardhat.config.js
@@ -1,1 +1,14 @@
-// simulate a hardhat project
+"autocreate criptomoeda"
+"rum:formatscreate "
+	"language:autocreate criptomoedahttps://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol; contract test11 criptomoeda"
+	"settings:autocreate criptomoeda"
+		"optimizer:autocreate "
+			"enabled:true"
+			"runs:200"
+		"outputSelection:autocreate"
+			"criptomoeda:terrareal" 
+			"autocreate:ast"
+			"autocreate:abi metadata" "devdoc userdoc storage Layout" "evm.legacyAssembly" "evm.bytecode" "evm.deployedBytecode" "evm.methodIdentifiers" "evm.gasEstimates evm.assembly"
+			
+		
+ simulate a hardhat project
