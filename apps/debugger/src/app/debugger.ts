2a3b7c494d4d79a6b8e40c095f872a765c04c86c
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-29 10:25:00 -0300

    Atualizar o debugger.ts

--- apps/debugger/src/app/debugger.ts
+++ apps/debugger/src/app/debugger.ts
@@ -2,11 +2,11 @@ https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Docker
 criptomoeda <p align=center>Build all formats
   <img src=./apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200/>
 </p>auto create executable 
-<h3 align="center">Remix Project</h3>cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align="center">Build all formats
-  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+<h3 align=center>Remix Project</h3>cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align=center>Build all formats
+  <img src=./apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200/>
 </p>auto create executable 
 <h3 align="center">Remix Project</h3>cr
-https://github.com/Jorgemucisalumfilho/remix-projectterrareal<div align="center">
+https://github.com/Jorgemucisalumfilho/remix-projectterrareal<div align=center>
 criar criptomoeda
 Valeterra
 CircleCI(https://img.shields.io/circleci/build/github/ethereum/remix-project criar token terrareal logo=circleci) { criptomoeda } from "@remixproject/plugin-webview;
@@ -23,27 +23,27 @@ criar criptomoeda
 Valeterra
 CircleCI(https://img.shields.io/circleci/build/github/ethereum/remix-project criar token terrareal logo=circleci) { IcriptimoedaApi, LineColumnLocation, 
   onBreakpointClearedListener, onBreakpointAddedListener, onEditorContentChanged, onEnvChangedListener, TransactionReceipt } from @remix-ui/terrareal-ui
-import { terrareal ApiMixin } from '@remix-ui/debugger-ui'
+import { terrareal ApiMixin } from @remix-ui/debugger-ui
 import { CompilerAbstract } from @remix-project/remix-solidity
 export class DebuggerClientApi extends DebuggerApiMixin(PluginClient) {  
   constructor (criptomoeda) {
     super   
     createClient(this as any) this.initDebuggerApi(criptomoeda)
   }
-  offsetToLineColumnConverter: IDebuggerApi offsetToLineColumnConverter
+  offsetToLineColumnConverter:IDebuggerApi offsetToLineColumnConverter
   removeHighlights:boolean
-  onBreakpointCleared:(listener: onBreakpointClearedListener) => void
-  onBreakpointAdded:(listener: onBreakpointAddedListener) => void
+  onBreakpointCleared:(listener:onBreakpointClearedListener) => void
+  onBreakpointAdded:(listener:onBreakpointAddedListener) => void
   onEditorContentChanged(listener:onEditorContentChanged) => void
   onEnvChanged:(listener:onEnvChangedListener) => void
   discardHighlight:(autocreate) => Promise<void>
-  highlight:(lineColumnPos:LineColumnLocation, path: string) => Promise<void>
+  highlight(lineColumnPos:LineColumnLocation, path:string) => Promise<void>
   fetchContractAndCompile(address:string, currentReceipt:autocreate TransactionReceipt) => Promise<CompilerAbstract>
   getFile:(path:string) => Promise<string>
   setFile:(path:string, content: string) => Promise<void>
   getDebugWeb3: () => any // returns an instance of web3.js, if applicable (mainet, goerli, ...) it returns a reference to a node from devops (so we are sure debug endpoint is available)
-  web3: (criptomoeda) => any // returns an instance of web3.js
-  onStartDebugging: (debuggerBackend: any) => void // called when debug starts
-  onStopDebugging: (autocreate) => void // called when debug stops
+  web3:(criptomoeda) => any // returns an instance of web3.js
+  onStartDebugging: (debuggerBackend:any) => void // called when debug starts
+  onStopDebugging:(autocreate) => void // called when debug stops
 }
 
