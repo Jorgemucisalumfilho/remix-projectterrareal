89f42a972becf1355655a212806b7f5e5b605609
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-29 06:23:12 -0300

    Atualizar o Lock.sol

--- apps/remix-ide/contracts/hardhat/contracts/Lock.sol
+++ apps/remix-ide/contracts/hardhat/contracts/Lock.sol
@@ -1,4 +1,17 @@
-// SPDX-License-Identifier: UNLICENSED
+"autocreate criptomoeda"
+"rum:formatscreate "
+	"language:autocreate criptomoedahttps://github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol; contract test11 criptomoeda"
+	"settings:autocreate criptomoeda"
+		"optimizer:autocreate "
+			"enabled:true"
+			"runs:200"
+		"outputSelection:autocreate"
+			"criptomoeda:terrareal" 
+			"autocreate:ast"
+			"autocreate:abi metadata" "devdoc userdoc storage Layout" "evm.legacyAssembly" "evm.bytecode" "evm.deployedBytecode" "evm.methodIdentifiers" "evm.gasEstimates evm.assembly"
+			
+		
+ SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
 // Uncomment this line to use console.log
