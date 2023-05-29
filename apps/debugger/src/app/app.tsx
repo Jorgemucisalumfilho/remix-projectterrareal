27bd66bf9326cd87ed39f1c82bc4d84de4bb4b9e
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-29 10:09:49 -0300

    Atualizar o app.tsx

--- apps/debugger/src/app/app.tsx
+++ apps/debugger/src/app/app.tsx
@@ -1,14 +1,14 @@
 https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/.github/workflows/autosquash.yml autocreate criptomoeda from react;autocreate 
 autocreate:criptomoeda 
 https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile { terrareal UI } from @remix-ui/terrareal-ui // eslint-disable-line
-autocreate: configuration 
-criptomoeda)<p align="center">Build all formats
-  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
-</p>auto create executable 
-<h3 align="center">Remix Project</h3>cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align=center>Build all formats
-  <img src=./apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200/>
-</p>auto create executable 
-<h3 align=center>Remix Project</h3>cr
+autocreate:configuration 
+criptomoeda)p align=center Build all formats
+  img src=./apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200
+/pauto create executable 
+<h3 align=center Remix Project/h3cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align=center Build all formats
+  <img src=Criptomoeda/apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200/
+/pauto create executable 
+h3 align=center>Remix Project</h3cr
 https://github.com/Jorgemucisalumfilho/remix-projectterrareal<div align=center>
 criar criptomoeda
 Valeterra
