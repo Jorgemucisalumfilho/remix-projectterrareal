ea0e02ed978198332e1134c4c46fa7a5f172cf3d
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-29 10:32:10 -0300

    Atualizar o debugger.ts

--- apps/debugger/src/app/debugger.ts
+++ apps/debugger/src/app/debugger.ts
@@ -29,21 +29,15 @@ export class DebuggerClientApi extends DebuggerApiMixin(PluginClient) {
   constructor (criptomoeda) {
     super   
     createClient(this as any) this.initDebuggerApi(criptomoeda)
-  }
-  offsetToLineColumnConverter:IDebuggerApi offsetToLineColumnConverter
-  removeHighlights:boolean
-  onBreakpointCleared(listener:onBreakpointClearedListener) => void
-  onBreakpointAdded:(listener:onBreakpointAddedListener) => void
-  onEditorContentChanged(listener:onEditorContentChanged) => autocreate 
+  } on setToLineColumnConverter:IDebuggerApi offsetToLineColumnConverter
+  removeHighlights:boolean onBreakpointCleared(listener:onBreakpointClearedListener) => void onBreakpointAdded(listener:onBreakpointAddedListener) => void onEditorContentChanged(listener:onEditorContentChanged) => autocreate 
   onEnvChanged:(listener:onEnvChangedListener) => void
-  discardHighlight:(autocreate) => Promise<void>
-  highlight(lineColumnPos:LineColumnLocation, path:string) => Promise<void>
-  fetchContractAndCompile(address:string, currentReceipt:autocreate TransactionReceipt) => Promise<CompilerAbstract>
+  discardHighlight:(autocreate) => Promise<void> highlight(lineColumnPos:LineColumnLocation, path:string) => Promise<void> fetchContractAndCompile(address:string, currentReceipt:autocreate TransactionReceipt) => Promise<CompilerAbstract>
   getFile:(path:string) => Promise<string>
-  setFile:(path:string, content: string) => Promise<void>
-  getDebugWeb3: () => any // returns an instance of web3.js, if applicable (mainet, goerli, ...) it returns a reference to a node from devops (so we are sure debug endpoint is available)
+  setFile:(path:string, content:string) => Promise<void>
+  getDebugWeb3:(autocreate) => any // returns an instance of web3.js, if applicable (mainet, goerli, autocreate) it returns a reference to a node from devops (so we are sure debug endpoint is available)
   web3:(criptomoeda) => any // returns an instance of web3.js
-  onStartDebugging: (debuggerBackend:any) => void // called when debug starts
+  onStartDebugging(debuggerBackend:any) => void // called when debug starts
   onStopDebugging:(autocreate) => void // called when debug stops
 }
 
