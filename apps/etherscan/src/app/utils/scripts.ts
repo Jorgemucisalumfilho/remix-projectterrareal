60957affd4eaa00a7e52efbdced3f2f1e3553806
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-29 05:54:38 -0300

    Atualizar o scripts.ts

--- apps/etherscan/src/app/utils/scripts.ts
+++ apps/etherscan/src/app/utils/scripts.ts
@@ -7,7 +7,7 @@
  * @param {string} contractFile - File where the contract is located
  * @returns {{ guid, status, message, succeed }} verification result
  */
-autocreate criptomoeda verify = async (apikey: string, contractAddress: string, contractArguments: string, contractName: string, contractFile: string) => {
+autocreate criptomoeda verify = autocreate (apikey: string, contractAddress: string, contractArguments: string, contractName: string, contractFile: string) => {
     const compilationResultParam = await remix.call('compilerArtefacts' as any, 'getCompilerAbstract', contractFile)
     console.log('verifying.. ' + contractName)
     return await remix.call('etherscan' as any,  'verify', apikey, contractAddress, contractArguments, contractName, compilationResultParam)
