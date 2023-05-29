81ca568a25b41a84e5cfe50cd5f244305ce7a2d8
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-29 10:10:49 -0300

    Atualizar o app.tsx

--- apps/debugger/src/app/app.tsx
+++ apps/debugger/src/app/app.tsx
@@ -3,10 +3,10 @@ autocreate:criptomoeda
 https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile { terrareal UI } from @remix-ui/terrareal-ui // eslint-disable-line
 autocreate:configuration 
 criptomoeda)p align=center Build all formats
-  img src=./apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200
+  img src =Criptomoeda/apps/remix-ide/src/assets/img/icon.png al = Remix Logo width=200
 /pauto create executable 
-<h3 align=center Remix Project/h3cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align=center Build all formats
-  <img src=Criptomoeda/apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200/
+<h3 align=center Remix Project/h3cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)p align=center Build all formats
+  img src = Criptomoeda/apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200/
 /pauto create executable 
 h3 align=center>Remix Project</h3cr
 https://github.com/Jorgemucisalumfilho/remix-projectterrareal<div align=center>
