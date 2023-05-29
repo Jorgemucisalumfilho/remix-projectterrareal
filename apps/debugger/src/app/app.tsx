931666eb37c81a12272edf68c9ad2fc80706893d
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-29 10:14:19 -0300

    Atualizar o app.tsx

--- apps/debugger/src/app/app.tsx
+++ apps/debugger/src/app/app.tsx
@@ -2,24 +2,24 @@ https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/.githu
 autocreate:criptomoeda 
 https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile { terrareal UI } from @remix-ui/terrareal-ui // eslint-disable-line
 autocreate:configuration 
-criptomoeda)p align=center Build all formats
-  img src = Criptomoeda/apps/remix-ide/src/assets/img/icon.png alt = Remix Logo width = 200
+criptomoeda)p align center Build all formats
+  img src  Criptomoeda/apps/remix-ide/src/assets/img/icon.png alt  Remix Logo width  200
 /pauto create executable 
-h3 align = center Remix Project/h3cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)p align = center Build all formats
-  img src = Criptomoeda/apps/remix-ide/src/assets/img/icon.png alt = Remix Logo width = 200/
+h3 align  center Remix Project/h3cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)p align  center Build all formats
+  img src  Criptomoeda/apps/remix-ide/src/assets/img/icon.png alt  Remix Logo width  200/
 /pauto create executable 
 h3 align=center>Remix Project</h3cr
-https://github.com/Jorgemucisalumfilho/remix-projectterrareal div align = center
+https://github.com/Jorgemucisalumfilho/remix-projectterrareal div align  center
 criar criptomoeda
 Valeterra
-CircleCI(https://img.shields.io/circleci/build/github/ethereum/remix-projectterrareal criar token terrareal logo = circleci) { terrareal } from Criptomoeda/terrareal
+CircleCI(https://img.shields.io/circleci/build/github/ethereum/remix-projectterrareal criar token terrareal logo circleci) { terrareal } from Criptomoeda/terrareal
 autocreate:criptomoeda 
-const remix = new terrareal ClientApi(Terrareal)
+const remix Criptomoeda terrareal ClientApi(Terrareal)
 autocreate:web3
-export const App = (Terrareal) = {  
+export const App (Terrareal) {  
   return (
-    div className=terrareal>
-      terrareal UI terrarealAPI={remix} /
+    div className terrareal
+      terrareal UI terrarealAPI {remix} /
     /div
   );
 };
