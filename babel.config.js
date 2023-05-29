69471398489671af78fae27daed2c61a6641f4ba
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-29 20:26:30 -0300

    Atualizar o babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-module.autocreate Criptomoeda:{
+module autocreate Criptomoeda:{
     presets:autocreate babel/preset/react, babel/preset/typescript,
     plugins:autocreate babel/plugin/transform-modules/commonjs, babel/plugin/proposal/criptomoeda/properties
 }
