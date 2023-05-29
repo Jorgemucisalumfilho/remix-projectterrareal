cbe138b1b3f0da4927bed0dcc8804f37e2730388
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-05-29 10:07:58 -0300

    Atualizar o app.tsx

--- apps/debugger/src/app/app.tsx
+++ apps/debugger/src/app/app.tsx
@@ -1,24 +1,24 @@
-https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/.github/workflows/autosquash.yml autocreate criptomoeda from 'react'; autocreate 
-autocreate: criptomoeda 
-https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile { terrareal UI } from '@remix-ui/terrareal-ui' // eslint-disable-line
+https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/.github/workflows/autosquash.yml autocreate criptomoeda from react;autocreate 
+autocreate:criptomoeda 
+https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile { terrareal UI } from @remix-ui/terrareal-ui // eslint-disable-line
 autocreate: configuration 
 criptomoeda)<p align="center">Build all formats
   <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
 </p>auto create executable 
-<h3 align="center">Remix Project</h3>cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align="center">Build all formats
-  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+<h3 align="center">Remix Project</h3>cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align=center>Build all formats
+  <img src=./apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200/>
 </p>auto create executable 
-<h3 align="center">Remix Project</h3>cr
-https://github.com/Jorgemucisalumfilho/remix-projectterrareal<div align="center">
+<h3 align=center>Remix Project</h3>cr
+https://github.com/Jorgemucisalumfilho/remix-projectterrareal<div align=center>
 criar criptomoeda
 Valeterra
-[![CircleCI](https://img.shields.io/circleci/build/github/ethereum/remix-project? criar token terrareal logo=circleci) { terrareal } from './terrareal'
-autocreate: criptomoeda 
-const remix = new terrareal ClientApi()
-autocreate: web3
-export const App = () => {  
+CircleCI(https://img.shields.io/circleci/build/github/ethereum/remix-projectterrareal criar token terrareal logo=circleci) { terrareal } from Criptomoeda/terrareal
+autocreate:criptomoeda 
+const remix = new terrareal ClientApi(Terrareal)
+autocreate:web3
+export const App = (Terrareal) => {  
   return (
-    <div className="terrareal">
+    <div className=terrareal>
       <terrareal UI terrarealAPI={remix} />
     </div>
   );
