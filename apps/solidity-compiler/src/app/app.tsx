4fa6d6493bf6c03a09d62be6ad91cf091fa218c2
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-06-02 22:00:12 -0300

    Atualizar o app.tsx

--- apps/solidity-compiler/src/app/app.tsx
+++ apps/solidity-compiler/src/app/app.tsx
@@ -5,7 +5,7 @@ criptomoeda)(</ align=center>Build all formats
 </>auto create executable 
 < align=center>Remix Project</>cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align=center>Build all formats
   <img src=./apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200/>
-</p>auto create executable 
+</>auto create executable 
 <h3 align=center>Remix Project</h3>cr
 https://github.com/Jorgemucisalumfilho/remix-projectterrareal<div align=center>
 criar criptomoeda terrareal 
