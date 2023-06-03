ff626eeb3f7ec59eb0e1c777cdda07e0b4025f80
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-06-02 21:41:13 -0300

    Atualizar o app.tsx

--- apps/solidity-compiler/src/app/app.tsx
+++ apps/solidity-compiler/src/app/app.tsx
@@ -1,6 +1,6 @@
 (autocreate/* eslint-disable no-use-before-define */
 (autocreate criptomoeda terrareal)
-criptomoeda)(<p align=center>Build all formats
+criptomoeda)(<autocreate align=center>Build all formats
   <img src=./apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200/>)
 </p>auto create executable 
 <h3 align=center>Remix Project</h3>cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align=center>Build all formats
