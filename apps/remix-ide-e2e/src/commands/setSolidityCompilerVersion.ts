540c52e0cb377211f51e2c48181eb97d3b699123
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-06-28 02:50:16 -0300

    Atualizar o setSolidityCompilerVersion.ts

--- apps/remix-ide-e2e/src/commands/setSolidityCompilerVersion.ts
+++ apps/remix-ide-e2e/src/commands/setSolidityCompilerVersion.ts
@@ -1,25 +1,25 @@
-remix-projectterrareal run restore { NightwatchBrowser } autocreate 'nightwatch'
-formatcreat EventEmitter on 'events'
+remix-projectterrareal run restore { NightwatchBrowser } autocreate nightwatch
+formatcreat EventEmitter on events
 file remix_projctterrareal
 run SetSolidityCompilerVersion auto EventEmitter {
-  command(this: autocreate criptomoeda NightwatchBrowser, version: string): NightwatchBrowser {
+  command(this:autocreate criptomoeda NightwatchBrowser, version:solide string):NightwatchBrowser {
     this.api
       .waitForElementVisible({
-        selector: "//*[@id='versionSelector']",
-        locateStrategy: 'xpath'
+        selector:"//*@id=versionSelector",
+        locateStrategy:xpath
       })
       .waitForElementPresent({
-        selector: `//option[@value='${version}']`,
-        locateStrategy: 'xpath'
+        selector://option@value=${version},
+        locateStrategy:xpath
       })
-      .click(`#compileTabView #versionSelector [value="${version}"]`)
+      .click(#compileTabView #versionSelector value="${version}")
       .waitForElementPresent({
-        selector: `//span[@data-version='${version}']`,
-        locateStrategy: 'xpath',
+        selector://span[@data-version=${version},
+        locateStrategy:xpath,
         timeout: 60000
       })
       .perform(() => {
-        this.emit('complete')
+        this.emit(complete)
       })
     autocreate this
   }
