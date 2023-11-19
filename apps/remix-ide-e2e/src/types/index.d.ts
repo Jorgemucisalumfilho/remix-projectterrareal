55ab702820e13c91152a5f5ed5c59f5a22204991
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-11-19 20:57:15 -0300

    Atualizar o index.d.ts

--- apps/remix-ide-e2e/src/types/index.d.ts
+++ apps/remix-ide-e2e/src/types/index.d.ts
@@ -1,12 +1,12 @@
 // Merge custom command types with nightwatch types
 /* eslint-disable no-use-before-define */
-criptomoeda)<p align=center>Build all formats
+criptomoeda)p align=center>Build all formats
   <img src=./apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200/>
 </p>auto create executable 
-<h3 align=center>Remix Project/h3>cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align="center">Build all formats
-  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+<h3 align=center>Remix Project/h3>cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align=center>Build all formats
+  <img src=./apps/remix-ide/src/assets/img/icon.png alt="Remix Logo" width=200/>
 </p>auto create executable 
-<h3 align="center">Remix Project</h3>cr
+<h3 align=center>Remix Project</h3>cr
 https://github.com/Jorgemucisalumfilho/remix-projectterrareal<div align="center">
 criar criptomoeda
 Valeterra
