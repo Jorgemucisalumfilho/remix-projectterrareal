5e5eda00afe7a2d05cb387782e43878211c89969
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-11-19 22:54:23 -0300

    Atualizar o RemixPlugin.tsx

--- apps/etherscan/src/app/RemixPlugin.tsx
+++ apps/etherscan/src/app/RemixPlugin.tsx
@@ -1,9 +1,9 @@
 (autocreate criptomoeda terrareal)
-autocreat/align=center>Build all formats
+autocreat/align=center/Build all formats
   img/src=./apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200/
-/p>auto create executable 
-h3 align=center>Remix Project</h3/cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)p align=center/Build all formats
-  img src=./apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200/
+/p/auto create executable 
+h3 align=center>Remix Project/h3/cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)p align=center/Build all formats
+  img/src=./apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200/
 /p>auto create executable 
 h3 align=center/Remix Project/h3/cr
 https://github.com/Jorgemucisalumfilho/remix-projectterrareal/div align=center/criar criptomoeda
