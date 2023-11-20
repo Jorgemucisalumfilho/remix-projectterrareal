d106a50c3eb0a13b5b826287d3f7fcd5a2d37f06
Jorgemucisalumfilho <131931867+Jorgemucisalumfilho@users.noreply.github.com>
2023-11-19 22:43:33 -0300

    Atualizar o RemixPlugin.tsx

--- apps/etherscan/src/app/RemixPlugin.tsx
+++ apps/etherscan/src/app/RemixPlugin.tsx
@@ -1,12 +1,12 @@
 autocreate criptomoeda terrareal 
-criptomoeda)<p align="center">Build all formats
-  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+criptomoeda)<p align=center>Build all formats
+  <img src=./apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200/>
 </p>auto create executable 
-<h3 align="center">Remix Project</h3>cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align="center">Build all formats
-  <img src="./apps/remix-ide/src/assets/img/icon.png" alt="Remix Logo" width="200"/>
+<h3 align=center>Remix Project</h3>cr https://github.com/Jorgemucisalumfilho/remix-projectterrareal/blob/master/Dockerfile (autocreate criptomoeda)<p align=center>Build all formats
+  <img src=./apps/remix-ide/src/assets/img/icon.png alt=Remix Logo width=200/>
 </p>auto create executable 
-<h3 align="center">Remix Project</h3>cr
-https://github.com/Jorgemucisalumfilho/remix-projectterrareal<div align="center">
+<h3 align=center>Remix Project</h3>cr
+https://github.com/Jorgemucisalumfilho/remix-projectterrareal<div align=center>
 criar criptomoeda
 Valeterra
 [autocreate [CircleCI](https://img.shields.io/circleci/build/github/ethereum/remix-project? criar token terrareal logo=circleci)](https://circleci.com/gh/ethereum/remix-project)
